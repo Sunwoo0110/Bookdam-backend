5f90d81b73c12daf6d6d708e8bf36adcae10c6a0
sunwookim <sunkim09@g.skku.edu>
2025-07-09 23:49:53 +0900

    FEAT init spring project

---
+++ bookdam/src/main/java/com/example/bookdam/BookdamApplication.java
@@ -0,0 +1,13 @@
+package com.example.bookdam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookdamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookdamApplication.class, args);
+	}
+
+}
---
+++ bookdam/src/test/java/com/example/bookdam/BookdamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bookdam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookdamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
